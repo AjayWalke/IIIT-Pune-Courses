17a5412340eac9c28bd61abb093887c6769f0904
Ajay Walke <84366358+AjayWalke@users.noreply.github.com>
2021-09-01 12:08:28 +0530

    Create problem-1.c

---
+++ 1st Semester.file/C Programming Lab Problems.file/Lab-1-problem.file/problem-1.c
@@ -0,0 +1,24 @@
+/*
+Write A Program To Display Following Output Using Less Number Of Lines
+-------------------------Record of Students-------------------
+Name            MIS NO          Subject         Marks
+Amit            12314           physics         90
+Sumit           12315           physics         95
+Sam             12316           physics         96
+-------------------------End of records-----------------------
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+  printf("-------------------------Record of Students-------------------\n");
+  printf("Name\t\tMIS NO\t\tSubject\t\tMarks\n");
+  printf("Amit\t\t12314\t\tphysics\t\t90\n");
+  printf("Sumit\t\t12315\t\tphysics\t\t95\n");
+  printf("Sam\t\t12316\t\tphysics\t\t96\n");
+  printf("-------------------------End of records-----------------------\n");
+  return 0;
+}
